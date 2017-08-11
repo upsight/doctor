d611e012df9717b6219acf6a1ad35f06a208275b
Aaron Godfrey <aaron.godfrey@upsight.com>
2017-08-11 09:50:05 -0700

    Fixing typo

--- doctor/response.py
+++ doctor/response.py
@@ -1,7 +1,7 @@
 class Response(object):
     """Represents a response.
 
-    This object contains the response itsetlf along with any additional headers
+    This object contains the response itself along with any additional headers
     that should be added and returned with the response data.  An instance of
     this class can be returned from a logic function in order to modify
     response headers.

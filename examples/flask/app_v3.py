c43b6039c15fe3d50068dc8d3b8e63589b2cb61e
Aaron Godfrey <aaron.godfrey@upsight.com>
2018-02-22 08:30:42 -0800

    Moved the new example app into it's own file.
    Modifying the existing app caused test failures and I'm not ready to
    address documentation just yet.

--- examples/flask/app_v3.py
+++ examples/flask/app_v3.py
@@ -2,38 +2,56 @@
 # generate documentation from this file. You're probably better served reading
 # the actual documentation (see Using in Flask in the docs).
 
-import os
-
 from flask import Flask
 from flask_restful import Api
 from doctor.errors import NotFoundError
-from doctor.flask import FlaskRouter
+from doctor.router_v3 import create_routes, get, post, put, delete
+from doctor.types import array, boolean, integer, string, Object
+
+# -- mark-types
+
+Body = string('Note body')
+Done = boolean('Marks if a note is done or not.')
+NoteId = integer('Note ID')
+Status = string('API status')
+
+
+class Note(Object):
+    description = 'A note object'
+    properties = {
+        'note_id': NoteId,
+        'body': Body,
+        'done': Done,
+    }
+
+
+Notes = array('Array of notes', items=Note)
 
 # -- mark-logic
 
 note = {'note_id': 1, 'body': 'Example body', 'done': False}
 
 
-def get_note(note_id):
+def get_note(note_id: NoteId) -> Note:
     """Get a note by ID."""
     if note_id != 1:
         raise NotFoundError('Note does not exist')
     return note
 
 
-def get_notes():
+def get_notes() -> Notes:
     """Get a list of notes."""
     return [note]
 
 
-def create_note(body, done=False):
+def create_note(body: Body, done: Done=False) -> Note:
     """Create a new note."""
     return {'note_id': 2,
             'body': body,
             'done': done}
 
 
-def update_note(note_id, body=None, done=None):
+def update_note(note_id: NoteId, body: Body=None, done: Done=None) -> Note:
     """Update an existing note."""
     if note_id != 1:
         raise NotFoundError('Note does not exist')
@@ -45,60 +63,34 @@ def update_note(note_id, body=None, done=None):
     return new_note
 
 
-def delete_note(note_id):
+def delete_note(note_id: NoteId):
     """Delete an existing note."""
     if note_id != 1:
         raise NotFoundError('Note does not exist')
 
 
-def status():
+def status() -> Status:
     return 'Notes API v1.0.0'
 
 
 # -- mark-app
-router = FlaskRouter(os.path.abspath('..'))
-routes = router.create_routes('API Status', 'schema.yaml', {
-    '/': {
-        'get': {
-            'logic': status,
-        },
-    },
-})
-routes.extend(router.create_routes('Notes (v1)', 'schema.yaml', {
-    '/note/': {
-        'get': {
-            'additional_args': {
-                'optional': ['limit', 'offset'],
-                'required': ['auth'],
-            },
-            'logic': get_notes,
-            'response': 'notes',
-            'title': 'Retrieve List',
-        },
-        'post': {
-            'logic': create_note,
-            'response': 'note',
-        },
-    },
-    '/note/<int:note_id>/': {
-        'delete': {
-            'logic': delete_note,
-        },
-        'get': {
-            'logic': get_note,
-            'response': 'note',
-        },
-        'put': {
-            'logic': update_note,
-            'response': 'note',
-        },
-    },
-}))
+
+routes = (
+    ('/', (
+        get(status),)),
+    ('/note/', (
+        get(get_notes),
+        post(create_note))),
+    ('/note/<int:note_id>/', (
+        delete(delete_note),
+        get(get_note),
+        put(update_note))),
+)
 
 app = Flask('Doctor example')
 
 api = Api(app)
-for route, resource in routes:
+for route, resource in create_routes(routes):
     api.add_resource(resource, route)
 
 if __name__ == '__main__':
